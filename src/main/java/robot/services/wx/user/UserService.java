002ff925c1f1b9640ef8ccc5c1bbcdabdb945075
tengjun <870409697@qq.com>
2020-03-25 11:44:27 +0800

    完善很多

--- src/main/java/robot/services/wx/user/UserService.java
+++ src/main/java/robot/services/wx/user/UserService.java
@@ -12,7 +12,7 @@ public class UserService {
     private UserMapper userMapper;
 
     public WXUser getWXUser(){
-        System.out.println("SSSS");
+        System.out.println("SSSSS");
         return new WXUser();
     }
 
